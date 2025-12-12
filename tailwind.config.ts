848f3e7de3769aac3c42c75437964b4e7a3d322e
VerburgtJimmy <jimmyverburgt@gmail.com>
2025-12-12 22:25:48 +0100

    refactor: remove Biomefile, format code in various files, and add biome configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js,tsx}"], // Scan these files for Tailwind classes
-	theme: {
-		extend: {},
-	},
-	plugins: [], // We'll add plugins here later if needed
+  content: ["./src/**/*.{html,js,tsx}"], // Scan these files for Tailwind classes
+  theme: {
+    extend: {},
+  },
+  plugins: [], // We'll add plugins here later if needed
 };
